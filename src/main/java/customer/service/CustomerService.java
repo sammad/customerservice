294cc84afd67fbb18faf97b38362f4fb9b161f3f
Madhur Sambhar <madhurs@tpt.com>
2017-03-24 18:16:20 +0530

    Added RestResource annotation

--- src/main/java/customer/service/CustomerService.java
+++ src/main/java/customer/service/CustomerService.java
@@ -2,8 +2,10 @@ package customer.service;
 
 import java.util.List;
 
-import customer.persistence.CustomerDTO;
+import org.springframework.data.rest.core.annotation.RestResource;
 
+import customer.persistence.CustomerDTO;
+@RestResource(path="/customers",rel="/customers")
 public interface CustomerService {
 
 	void delete(CustomerDTO customer);
